ceefd4e7ee4e87ec3f12ea2011f08742b039f8f9
welinton-cool <welinton.sacon@espoch.edu.ec>
2025-04-09 08:35:30 -0500

    holaaaa

---
+++ src/main/java/ec/edu/ec/w11111111/W11111111.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.w11111111;
+
+/**
+ *
+ * @author HP
+ */
+public class W11111111 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
